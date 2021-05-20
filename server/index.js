364fae54cf8f7f24050b53498c573ed66c1fb658
Mario Torres <48029021+marito07@users.noreply.github.com>
2021-05-20 22:27:30 +0200

    Changes heroku

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path');
 var bodyParser = require('body-parser');
 const app = express()
 const port = 3001
@@ -16,6 +17,12 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
 
 app.get("/list/:url", function (request, response) {
     var salida = callU.call('https://letterboxd.com/johncassavetes/list/old-films-for-people-who-want-to-watch-more/');

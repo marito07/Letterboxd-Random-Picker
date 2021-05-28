689be114d7abf1d82cd10110f0d895fdd4ae9dbe
Mario Torres <48029021+marito07@users.noreply.github.com>
2021-05-28 15:42:19 +0200

    Making the App Public - Code cleaning and Loading Spinner for the Request
    This is not yet finished, but the basic functionality is pretty much done.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Letterboxd-Random-Picker
-Randomly picks a movie out of a given Letterboxd list
+This Web Application was made using React, Express, Node.js and Cheerio.
+
+It randomly selects a movie out of a given Letterboxd List URL
+
+App Link: https://letterboxd-random-picker.herokuapp.com/
+
+(Note: this is a WIP, the visual aspect of the App is not finished)
--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 //import logo from "../public/logo.svg"
 import './App.css';
-import './bootstrap.css'
+import * as ReactBootstrap from 'react-bootstrap'
 
 function App() {
 
@@ -17,6 +17,7 @@ function App() {
 
   const [checkedDoc, setCheckedDoc] = React.useState(false);
   const [checkedShort, setCheckedShort] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
   const handleClickDoc = () => setCheckedDoc(!checkedDoc)
   const handleClickShort = () => setCheckedShort(!checkedShort)
@@ -25,7 +26,7 @@ function App() {
 
 
   const getRandomMovie = async (movieURL) => {
-    console.log(movieURL);
+    setLoading(true);
     const response = await fetch('/list', {
       method: 'post',
       headers: {'Content-Type':'application/json'},
@@ -46,11 +47,8 @@ function App() {
 
     var json2 = await response2.json();
     if(checkedShort && checkedDoc){
-      console.log(json2.duration)
-      console.log(json2.duration)
       while(json2.duration < 60 || json2.genres.includes('documentary')){
         var item2 = await json[Math.floor(Math.random() * json.length)];
-        console.log("NUEVO",item2)
         const response3 = await fetch('/getMovie', {
           method: 'post',
           headers: {'Content-Type':'application/json'},
@@ -58,18 +56,13 @@ function App() {
             "url": item2
           })
         });
-        console.log("sale de la llamada");
 
         json2 = await response3.json();
-        console.log("Double Check",json2)
       }
     }
     else if(checkedShort){
-      console.log('Cortos');
       while(json2.duration < 60){
-        console.log(json2.duration)
         var item2 = await json[Math.floor(Math.random() * json.length)];
-        console.log("NUEVO",item2)
         const response3 = await fetch('/getMovie', {
           method: 'post',
           headers: {'Content-Type':'application/json'},
@@ -77,18 +70,13 @@ function App() {
             "url": item2
           })
         });
-        console.log("sale de la llamada");
 
         json2 = await response3.json();
-        console.log("Duration",json2)
       }
     }
     else if(checkedDoc){
-      console.log('documentales');
       while(json2.genres.includes('documentary')){
-        console.log(json2.genres);
         var item2 = await json[Math.floor(Math.random() * json.length)];
-        console.log("NUEVO",item2)
         const response3 = await fetch('/getMovie', {
           method: 'post',
           headers: {'Content-Type':'application/json'},
@@ -96,9 +84,7 @@ function App() {
             "url": item2
           })
         });
-        console.log("sale de la llamada");
         json2 = await response3.json();
-        console.log("Doc",json2)
       }
       
     }
@@ -108,6 +94,7 @@ function App() {
     dataCopy = json2;
     setData( dataCopy );
     console.log(data)
+    setLoading(false);
 
 
   };
@@ -121,34 +108,35 @@ function App() {
         <h1>Letterboxd Random List Movie Picker</h1>
         <form onSubmit={handleSubmit(getRandomMovie)}> 
           <label class="mr-3" for="fname">List URL:</label>
-          <input type="text" id="fname" name="fname" {...register("url", {required: "Required"})}></input>
+          <input disabled={loading} type="text" id="fname" name="fname" {...register("url", {required: "Required"})}></input>
 
 
 
-          <input class="ml-2 btn btn-primary" type="submit" value="SEND"></input>
+          <input disabled={loading} class="ml-2 btn btn-primary" type="submit" value="SEND"></input>
           <div class="d-flex flex-row align-items-center">
             <label class="smol m-0 ml-5 mr-2">Exclude documentaries</label>
-            <input type="checkbox" onClick={handleClickDoc} onChange={handleClickDoc} checked={checkedDoc}></input>
+            <input disabled={loading} type="checkbox" onClick={handleClickDoc} onChange={handleClickDoc} checked={checkedDoc}></input>
           </div>
           <div class="d-flex flex-row align-items-center">
             <label class="smol m-0 ml-5 mr-2">Exclude short films</label>
-            <input type="checkbox" onClick={handleClickShort} onChange={handleClickShort} checked={checkedShort}></input>
+            <input disabled={loading} type="checkbox" onClick={handleClickShort} onChange={handleClickShort} checked={checkedShort}></input>
           </div>
 
         </form> 
-        <div>
+        {!loading ? <div>
           <br></br>
           {data.poster ? <a href={data.url}><img src={data.poster} ></img></a>:null}
           <br></br>
-          {data.url ? <a href={data.url}>{data.title}</a>: null}
+          {data.url ? <a href={data.url} target="_blank">{data.title}</a>: null}
           {data.year ? <p>({data.year})</p>: null}
           {data.duration ? <p>Duration: {data.duration}m</p>: null}
-          {data.genres.length > 0 ? <p>Genres: 
-            
-            {data.genres.map(i => <label class="ml-2 mr-2">{i}</label>)}
+          {data.genres.length > 0 ? <p> <span class="font-weight-bold">Genres: </span>
+            {data.genres.map(i => <label class="border border-light m-1 p-1">{i}</label>)}
             </p>: null}
 
-        </div>
+        </div> : <div class="mt-5"><ReactBootstrap.Spinner animation="border" variant="light" /></div>}
+
+
       </header>
      </div>
     
